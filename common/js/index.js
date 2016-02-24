8f8a8ada0d79f9cce37424a0364925de5ba534e0
Fran Lapuente <franlapuentedev@gmail.com>
2016-02-24 18:53:29 +0100

    Eliminados ficheros no utilizados

--- common/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    
-    $("#btn-login").click(function(e){
-        e.preventDefault();
-        console.log("hola");
-        window.location = "list.html";
-    });
-    
-});
